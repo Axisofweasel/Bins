3018f55677afe75cec39aec6d487ec194af3871a
Chris Hinds <c.hinds09@googlemail.com>
2024-06-26 14:03:40 +0100

    Getting Binflow up and running

--- Flows/BinFlow.py
+++ Flows/BinFlow.py
@@ -1,31 +1,84 @@
+import datetime
 import dotenv
-import logging
 import requests
+import re
 import os
-from prefect import flow
+from bs4 import BeautifulSoup
+from prefect import task, flow, get_run_logger, runtime
 
-@flow(log_prints=True)
-def binflow():
+@task(log_prints=True)
+def calendar_check():
     
-    dotenv.load_dotenv()
+    logger = get_run_logger()
+    logger.info("INFO level logging")
+    logger.info("You can see this becausse we're logging DEBUG errors")
 
-    logging.basicConfig(
-        filename= './app.log',
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-    )
+    bin_url = os.getenv('BINURL')
+    debug = int(os.getenv('DEBUG', '0')) 
 
-    api_token = os.getenv('PushoverApiToken')
-    user_token = os.getenv('UserToken')
-    api_url = os.getenv('ApiUrl')
+    bins ={
+            'Your next Blue Bin day': ['Blue Bin','#0000FF'],
+            'Your next Green Bin day': ['Green Bin','#008000'],
+            'Your next Brown Bin day': ['Brown Bin','#A52A2A'],
+            'Your next Purple Bin day': ['Purple Bin','#800080']
+            }
 
-    formatted_message = 'This is a test of the emergency bin system'
+    today = datetime.datetime.today()
+    tomorrow = today + datetime.timedelta(days = 1)
+    tomorrowdate = tomorrow.strftime('%Y-%m-%d')
+    
+    logger.debug('Getting Bin Calendar')
+    html = requests.get(url=bin_url)
+    sitesoup = BeautifulSoup(html._content, 'html.parser')
+    bin_html = sitesoup.find('fieldset')
+    child_html = bin_html.findChildren('p')
+    address = sitesoup.find(id="Application_AddressForUPRN")
+    address = address.text.strip().strip(',')
 
-    title = 'Test'
+    logger.debug('Formatting message')
+    messagelist = []
+    messagelist.append(f'<b>{address}</b>')
+    messagelist.append(f'<a href="{bin_url}">Bin Calendar</a>')
+    
+    send = False
+    
+    logger.debug('Checking dates for sending')
+    for c in child_html:
+        x = re.split('\\.', c.text.strip())
+        y = re.split(' is ', x[0])
+        if 'Today' in x[0]:
+            messagelist.append(f'<font color={bins[y[0]][1]}>Today: {bins[y[0]][0]}</font>')
+            send = True
+        else:
+            if 'Tomorrow' in y[1]: 
+                send = True
+                messagelist.append(f'<font color={bins[y[0]][1]}>Tomorrow: {bins[y[0]][0]}</font>')
+
+    formatted_message = '\n'.join(messagelist)
+    
+    if debug == 1:
+        logger.debug('Setting Debug Message')
+        send = True
+        formatted_message = 'Debugging Binfluencer Bot'
+
+    return formatted_message, send
+
+@task(log_prints=True)
+def message_post(formatted_message, send):
+    
+    logger = get_run_logger()
+    logger.info("INFO level logging")
+    logger.info("You can see this becausse we're logging DEBUG errors")
+
+    api_token = os.getenv('PUSHOVERAPITOKEN')
+    user_token = os.getenv('USERTOKEN')
+    api_url = os.getenv('APIURL')
+    
+    title = 'Bin Reminder'
     message = formatted_message
     html = 1
 
-    logging.info(f'{message}')
+    logger.info(f'{message}')
 
     message_dict = {
         'token': api_token,
@@ -35,23 +88,41 @@ def binflow():
         'html': html
     }
     
-    try:
-        x = requests.post(api_url, json=message_dict)
-        x.raise_for_status()
-        logging.info('POST successful')
-    except requests.exceptions.HTTPError as e:
-        logging.error(f'HTTP error occurred: {e}')
-    except requests.exceptions.ConnectionError as e:
-        logging.error(f'Connection error occurred: {e}')
-    except requests.exceptions.Timeout as e:
-        logging.error(f'Timeout error occurred: {e}')  # Fixed here
-    except requests.exceptions.RequestException as e:
-        logging.error(f'An error occurred: {e}')  # Fixed here
+    if send == True:
+        try:
+            logger.debug('Sending Message')
+            x = requests.post(api_url, json=message_dict)
+            x.raise_for_status()
+            logger.info('POST successful')
+        except requests.exceptions.HTTPError as e:
+            logger.error(f'HTTP error occurred: {e}')
+        except requests.exceptions.ConnectionError as e:
+            logger.error(f'Connection error occurred: {e}')
+        except requests.exceptions.Timeout as e:
+            logger.error(f'Timeout error occurred: {e}')  
+        except requests.exceptions.RequestException as e:
+            logger.error(f'An error occurred: {e}')  
+    return
+
+
+
+@flow(log_prints=True, retries=3, retry_delay_seconds=30)
+def binflow():
     
+    dotenv.load_dotenv()
+
+    logger = get_run_logger()
+    logger.info("INFO level logging")
+    logger.info("You can see this becausse we're logging DEBUG errors")
+    
+    print("My Name is", runtime.flow_run.name)
+    print("I'm part of deployment", runtime.deployment.name)
+    print("I should have started at", runtime.flow_run.scheduled_start_time)
+
+    formatted_message, send = calendar_check()
+    message_post(formatted_message, send)
     
 if __name__ =="__main__":
-    #binflow()
-    #binflow.serve()
     dotenv.load_dotenv()
     binflow.deploy(
         name = "Binfluencer Bot",
@@ -60,9 +131,10 @@ if __name__ =="__main__":
         push = False,
         tags=["Bins", "Chris"],
         job_variables={"env":{
-            "BinUrl": os.getenv('BinUrl'),
-"PushoverApiToken": os.getenv('PushoverApiToken'),
-"UserToken": os.getenv('UserToken'),
-"ApiUrl": os.getenv('ApiUrl'),
+            "BINURL": os.getenv('BINURL'),
+            "PUSHOVERAPITOKEN": os.getenv('PUSHOVERAPITOKEN'),
+            "USERTOKEN": os.getenv('USERTOKEN'),
+            "APIURL": os.getenv('APIURL'),
+            "DEBUG": os.getenv('DEBUG'),
         }}
     )
